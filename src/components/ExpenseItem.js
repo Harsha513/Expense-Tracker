d1a24760ee6244d9a3b47d60b185431118d3e9e0
HarshaBandla <harshabandla9014@gmail.com>
2023-06-21 11:08:48 -0500

    Build ExpenseItem Component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>
+                June 21st 2023
+            </div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>$294.31</div>
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseItem;
